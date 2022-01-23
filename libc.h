b6edb69919ace387398e926991338f97762b7f2f
cherrry9 <cherrry9@disroot.org>
2022-01-23 20:48:28 +0100

    Improve handling numerical arguments

--- libc.h
+++ libc.h
@@ -1,16 +1,13 @@
 #if 0
 #!/bin/sh
-#
-# Plan 9 like libc.h
-#
 
 set -xe
 
-SRC=
-BIN=
+SRC=main.c
+BIN=main
 
 CPPFLAGS="-D_DEFAULT_SOURCE"
-CFLAGS="-ggdb -ansi -pedantic -Wextra -Wall $CPPFLAGS"
+CFLAGS="-ggdb -pedantic -Wextra -Wall $CPPFLAGS"
 LDFLAGS=
 CC=cc
 
@@ -19,7 +16,7 @@ if [ "$1" = clean ]; then
 		rm -f "${f%.c}.o"
 	done
 	rm -f "$BIN"
-	[ $# -eq 0 ] && exit 0
+	[ $# -eq 1 ] && exit 0
 fi
 
 for f in $SRC; do
@@ -45,6 +42,7 @@ exit 0
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <limits.h>
 #ifdef MEMCHECK_MUTEX
 #define MEMCHECK
 #include <pthread.h>
@@ -57,15 +55,74 @@ typedef unsigned long  ulong;
 
 #define MAX(x, y)         ((x) > (y) ? (x) : (y))
 #define MIN(x, y)         ((x) < (y) ? (x) : (y))
-#define BETWEEN(x, a, b)  ((a) <= (x) && (x) <= (b))
-#define IMPLIES(x, y)     (!(x) || (y))
-#define COMPARE(x, y)     (((x) > (y)) - ((x) < (y)))
-#define SIGN(x)           COMPARE(x, 0)
-#define SIZE(X)           (sizeof X / sizeof X[0]
-#define SWAP(x, y, T)     do { T tmp = (x); (x) = (y); (y) = tmp; } while (0)
+#define LIMIT(a, x, b)    MAX(MIN(x, b), x, a)
+#define SIZE(x)           (sizeof x / sizeof *x)
 #define ENTRY_OF(p, t, m) (t *)((char *)(p) - offsetof(t, m))
+#define MAX_ALIGN         0x10
+#define ALIGN(x, align)   (((x) + (align) - 1) & ~((align) - 1))
 
-extern void die(const char *fmt, ...);
+char *argv0;
+
+/* use main(int argc, char *argv[]) */
+#define ARGBEGIN                                                              \
+        char argc_;                                                           \
+        char **argv_;                                                         \
+        int brk_;                                                             \
+        for (argv0 = *argv, argv++, argc--;                                   \
+             *argv && (*argv)[0] == '-' && (*argv)[1];                        \
+             argc--, argv++) {                                                \
+                if ((*argv)[1] == '-' && (*argv)[2] == '\0') {                \
+                        argv++; argc--; break;                                \
+                }                                                             \
+                for (brk_ = 0, ++*argv, argv_ = argv;                         \
+                     **argv && !brk_; ++*argv) {                              \
+                        if (argv != argv_) break;                             \
+                        argc_ = **argv;                                       \
+                        switch (argc_)
+                        /*
+                        case 'a':
+                                opt_a = EARGF(usage());
+                                break;
+                         */
+
+#define ARGC() argc_
+
+/* handles obsolete -NUM syntax */
+#define ARGNUM                                                                \
+                        case '0':                                             \
+                        case '1':                                             \
+                        case '2':                                             \
+                        case '3':                                             \
+                        case '4':                                             \
+                        case '5':                                             \
+                        case '6':                                             \
+                        case '7':                                             \
+                        case '8':                                             \
+                        case '9'
+
+#define ARGNUMF() (brk_ = 1, (int)estrtonum(*argv, 0, INT_MAX))
+
+#define GETARG_(err)                                                          \
+        (((*argv)[1] == '\0' && argv[1] == NULL) ?                            \
+                (err) :                                                       \
+                (brk_ = 1, ((*argv)[1] != '\0') ?                             \
+                        (*argv + 1) :                                         \
+                        (argc--, *++argv)))
+
+
+#define ARGF()     GETARG_((char *)0)
+#define EARGF(efn) GETARG_((efn, abort(), (char *)0))
+
+#define EARGF2NUM(efn, min, max) (estrtonum(EARGF(efn), min, max))
+#define EARGF2UINT(efn)          (int)EARGF2NUM(efn, 0, INT_MAX)
+#define EARGF2INT(efn)           (int)EARGF2NUM(efn, INT_MIN, INT_MAX)
+
+#define ARGEND }}
+
+extern void eprintf(const char *fmt, ...);
+extern void enprintf(int status, const char *fmt, ...);
+extern void weprintf(const char *fmt, ...);
+extern long long estrtonum(const char *numstr, long long minval, long long maxval);
 extern void *emalloc(size_t size);
 extern void *erealloc(void *ptr, size_t size);
 extern void *ecalloc(size_t nmemb, size_t size);
@@ -85,19 +142,76 @@ extern void memdump(void);
 #ifdef LIBC_IMPLEMENTATION
 #undef LIBC_IMPLEMENTATION
 
+static void xvprintf(const char *fmt, va_list ap);
+
 void
-die(const char *fmt, ...)
+eprintf(const char *fmt, ...)
 {
 	va_list ap;
 
 	va_start(ap, fmt);
-	vfprintf(stderr, fmt, ap);
+	xvprintf(fmt, ap);
 	va_end(ap);
+
+	exit(1);
+}
+
+void
+enprintf(int status, const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	xvprintf(fmt, ap);
+	va_end(ap);
+
+	exit(status);
+}
+
+void
+weprintf(const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	xvprintf(fmt, ap);
+	va_end(ap);
+}
+
+void
+xvprintf(const char *fmt, va_list ap)
+{
+	if (argv0 && strncmp(fmt, "usage", strlen("usage")))
+		fprintf(stderr, "%s: ", argv0);
+
+	vfprintf(stderr, fmt, ap);
+
 	if (fmt[0] && fmt[strlen(fmt) - 1] == ':') {
 		fputc(' ', stderr);
 		perror(NULL);
 	} else fputc('\n', stderr);
-	exit(1);
+}
+
+long long
+estrtonum(const char *numstr, long long minval, long long maxval)
+{
+	long long ll;
+	char *ep;
+	const char *error;
+
+	assert(numstr != NULL);
+	assert(minval <= maxval);
+
+	error = NULL;
+	ll = strtoll(numstr, &ep, 10);
+	if (numstr == ep || *ep != '\0')
+		error = "invalid";
+	else if ((ll == LLONG_MIN && errno == ERANGE) || ll < minval)
+		error = "too small";
+	else if ((ll == LLONG_MAX && errno == ERANGE) || ll > maxval)
+		error = "too large";
+	if (error) eprintf("%s: %s number", numstr, error);
+	return ll;
 }
 
 void *
@@ -105,7 +219,7 @@ emalloc(size_t size)
 {
 	void *p;
 
-	if (!(p = malloc(size))) die("malloc:");
+	if (!(p = malloc(size))) eprintf("malloc:");
 	return p;
 }
 
@@ -114,7 +228,7 @@ erealloc(void *ptr, size_t size)
 {
 	void *p;
 
-	if (!(p = realloc(ptr, size))) die("realloc:");
+	if (!(p = realloc(ptr, size))) eprintf("realloc:");
 	return p;
 }
 
@@ -123,14 +237,14 @@ ecalloc(size_t nmemb, size_t size)
 {
 	void *p;
 
-	if (!(p = calloc(nmemb, size))) die("calloc:");
+	if (!(p = calloc(nmemb, size))) eprintf("calloc:");
 	return p;
 }
 
 char *
 estrdup(const char *str)
 {
-	if (!(str = strdup(str))) die("strdup:");
+	if (!(str = strdup(str))) eprintf("strdup:");
 	return (char *)str;
 }
 
@@ -164,8 +278,10 @@ void *
 mcmalloc(size_t size, const char *file, int line)
 {
 	alloc_info *alloc;
+	size_t alloc_align;
 
-	alloc = malloc(size + sizeof *alloc_head);
+	alloc_align = ALIGN(sizeof *alloc, MAX_ALIGN);
+	alloc = malloc(size + alloc_align);
 	if (!alloc) {
 		fprintf(stderr, "malloc: %s\n", strerror(errno));
 		memdump();
@@ -180,7 +296,7 @@ mcmalloc(size_t size, const char *file, int line)
 	if (alloc_head) alloc->next->prev = alloc;
 	alloc_head = alloc;
 	MEMCHECK_MUTEX_UNLOCK();
-	return alloc + 1;
+	return (char *)alloc + alloc_align;
 }
 
 void *
@@ -195,7 +311,7 @@ mcrealloc(void *ptr, size_t size, const char *file, int line)
 		return NULL;
 	}
 
-	alloc = (alloc_info *)ptr - 1;
+	alloc = (alloc_info *)((char *)ptr - ALIGN(sizeof *alloc, MAX_ALIGN));
 	if (size <= alloc->size) return ptr;
 #ifdef MEMCHECK_PRESERVE_FILELINE
 	new = mcmalloc(size, alloc->file, alloc->line);
@@ -233,7 +349,7 @@ mcfree(void *ptr)
 	alloc_info *alloc;
 
 	if (ptr == NULL) return;
-	alloc = (alloc_info *)ptr - 1;
+	alloc = (alloc_info *)((char *)ptr - ALIGN(sizeof *alloc, MAX_ALIGN));
 	alloc->size = ~alloc->size;
 #ifndef MEMCHECK_SHOWALL
 	MEMCHECK_MUTEX_LOCK();
@@ -261,7 +377,7 @@ memdump(void)
 			reason = "FREED ";
 		}
 		fprintf(
-		    MEMCHECK_PIPE, "[%s] %s:%4d: %lu bytes at %p\n", reason,
+		    MEMCHECK_PIPE, "[%s] %s:%d: %lu bytes at %p\n", reason,
 		    alloc->file, alloc->line, alloc->size, (void *)(alloc + 1)
 		);
 	}
@@ -271,60 +387,9 @@ memdump(void)
 #endif /* LIBC_IMPLEMENTATION */
 
 #ifdef MEMCHECK
-#define malloc(size)         mcmalloc(size,        __FILE__, __LINE__)
-#define calloc(nmemb, size)  mccalloc(nmemb, size, __FILE__, __LINE__)
-#define realloc(ptr, size)   mcrealloc(ptr, size,  __FILE__, __LINE__)
-#define strdup(str)          mcstrdup(str,         __FILE__, __LINE__)
-#define free(ptr)            mcfree(ptr)
-#else
-#define malloc  emalloc
-#define calloc  ecalloc
-#define realloc erealloc
-#define strdup  estrdup
+#define emalloc(size)         mcmalloc(size,        __FILE__, __LINE__)
+#define ecalloc(nmemb, size)  mccalloc(nmemb, size, __FILE__, __LINE__)
+#define erealloc(ptr, size)   mcrealloc(ptr, size,  __FILE__, __LINE__)
+#define estrdup(str)          mcstrdup(str,         __FILE__, __LINE__)
+#define efree(ptr)            mcfree(ptr)
 #endif
-
-extern char *argv0;
-
-/* use main(int argc, char *argv[]) */
-#define ARGBEGIN                                                              \
-        char argc_;                                                           \
-        char **argv_;                                                         \
-        int brk_;                                                             \
-        int i_;                                                               \
-        for (argv0 = *argv, argv++, argc--;                                   \
-             *argv && (*argv)[0] == '-' && (*argv)[1];                        \
-             argc--, argv++) {                                                \
-                if ((*argv)[1] == '-' && (*argv)[2] == '\0') {                \
-                        argv++; argc--; break;                                \
-                }                                                             \
-                for (i_ = 1, brk_ = 0, argv_ = argv;                          \
-                     (*argv)[i_] && !brk_; i_++) {                            \
-                        if (argv != argv_) break;                             \
-                        argc_ = (*argv)[i_];                                  \
-                        switch (argc_)
-                        /*
-                        case 'a':
-                                opt_a = ARGF();
-                                break;
-                        case 'b':
-                                opt_b = EARGF(usage());
-                                break;
-                         */
-#define ARGC() argc_
-
-#define ARGF()                                                                \
-        (((*argv)[i_+1] == '\0' && argv[1] == NULL) ?                         \
-                (char *)0 :                                                   \
-                (brk_ = 1, ((*argv)[i_+1] != '\0') ?                          \
-                        (&(*argv)[i_+1]) :                                    \
-                        (argc--, *++argv)))
-
-#define EARGF(x)                                                              \
-        (((*argv)[i_+1] == '\0' && argv[1] == NULL) ?                         \
-                ((x), abort(), (char *)0) :                                   \
-                (brk_ = 1, ((*argv)[i_+1] != '\0') ?                          \
-                        (&(*argv)[i_+1]) :                                    \
-                        (argc--, *++argv)))
-
-
-#define ARGEND }}
